9e2fecd7bcfe05d38c19e85d503f43626b21a6e2
Pabanthapa91 <aanayabthapa@gmail.com>
2020-08-18 18:04:01 -0400

    Test3 & 4 Class

---
+++ src/day02/task3_git.java
@@ -0,0 +1,7 @@
+package day02;
+
+public class task3_git {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/day02/task4_git.java
@@ -0,0 +1,7 @@
+package day02;
+
+public class task4_git {
+    public static void main(String[] args) {
+
+    }
+}
